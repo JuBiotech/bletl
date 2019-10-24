d3eb110af82fa11eddb6dc9a3640e458a66e52e4
Michael Osthege <m.osthege@fz-juelich.de>
2019-10-24 16:51:18 +0200

    test both spline methods for reproducibility within reasonable rages

--- bletl_analysis/tests/tests.py
+++ bletl_analysis/tests/tests.py
@@ -28,34 +28,34 @@ class TestSplineMueScipy(unittest.TestCase):
     def test_get_single_spline(self):
         """Tests the interpolation of backscatters works with an absolute tolerance of <0.1."""
         bldata = bletl.parse(FP_TESTFILE)
-        numpy.random.seed(9001)
 
         x, y = bldata['BS3'].get_timeseries('F05')
 
         spline = bletl_analysis.get_crossvalidated_spline(x, y, method='us')
         
         self.assertIsInstance(spline, scipy.interpolate.UnivariateSpline)
-        
+
         # the last point should be very close
-        numpy.testing.assert_allclose(spline(19.4275), 23.66, atol=0.01)
+        numpy.testing.assert_allclose(spline(19.4275), 23.66, atol=0.1)
 
         # a range of points at the end of the curve
         numpy.testing.assert_allclose(spline([18.8275, 19.0275, 19.22777778]), [23.75, 23.67, 23.68], atol=0.1)
         return
 
     def test_get_mue_wells(self):
-        """Tests that the growth rate at the end of the exponential phase is calculated with <0.001 absolute deviance."""
+        """Tests that the median growth rate over 15 exponential cycles is calculated with <0.02 absolute deviance."""
         bldata = bletl.parse(FP_TESTFILE)
-        numpy.random.seed(9001)
         wells = 'A01,A02,B03,C05'.split(',')
 
         # automatic blank
         mue_blank_first = bletl_analysis.get_mue(bldata['BS3'], wells=wells, method='us')
-        numpy.testing.assert_allclose(mue_blank_first.value.loc[50, 'B03'], 0.375, atol=0.001)
+        mue_median = numpy.median(mue_blank_first.value.loc[60:75, 'B03'])
+        numpy.testing.assert_allclose(mue_median, 0.38, atol=0.01)
 
         # scalar blank for all
         mue_blank_scalar = bletl_analysis.get_mue(bldata['BS3'], blank=2, wells=wells, method='us')
-        numpy.testing.assert_allclose(mue_blank_scalar.value.loc[50, 'B03'], 0.174, atol=0.001)
+        mue_median = numpy.median(mue_blank_scalar.value.loc[60:75, 'B03'])
+        numpy.testing.assert_allclose(mue_median, 0.30, atol=0.01)
         
         # dictionary of scalars (first 5 cycles)
         blank_dict = {
@@ -64,7 +64,8 @@ class TestSplineMueScipy(unittest.TestCase):
             if well in wells
         }
         mue_blank_dict = bletl_analysis.get_mue(bldata['BS3'], wells=wells, blank=blank_dict, method='us')
-        numpy.testing.assert_allclose(mue_blank_dict.value.loc[50, 'B03'], 0.369, atol=0.001)
+        mue_median = numpy.median(mue_blank_dict.value.loc[60:75, 'B03'])
+        numpy.testing.assert_allclose(mue_median, 0.38, atol=0.01)
 
         # check value error when the wells dictionary is incorrect
         with self.assertRaises(ValueError):
@@ -75,20 +76,21 @@ class TestSplineMueScipy(unittest.TestCase):
         return
 
     def test_get_mue_on_all(self):
-        """Tests that the growth rate at the end of the exponential phase is calculated with <0.001 absolute deviance."""
+        """Tests that the median growth rate over 15 exponential cycles is calculated with <0.02 absolute deviance."""
         bldata = bletl.parse(FP_TESTFILE)
         wells = list('A01,A02,B03,C05'.split(','))
         bldata['BS3'].time = bldata['BS3'].time[wells]
         bldata['BS3'].value = bldata['BS3'].value[wells]
-        numpy.random.seed(9001)
         
         # automatic
         mue_blank_first = bletl_analysis.get_mue(bldata['BS3'], method='us')
-        numpy.testing.assert_allclose(mue_blank_first.value.loc[50, 'B03'], 0.375, atol=0.001)
+        mue_median = numpy.median(mue_blank_first.value.loc[60:75, 'B03'])
+        numpy.testing.assert_allclose(mue_median, 0.38, atol=0.01)
 
         # scalar blank for all
         mue_blank_scalar = bletl_analysis.get_mue(bldata['BS3'], blank=2, method='us')
-        numpy.testing.assert_allclose(mue_blank_scalar.value.loc[50, 'B03'], 0.174, atol=0.001)
+        mue_median = numpy.median(mue_blank_scalar.value.loc[60:75, 'B03'])
+        numpy.testing.assert_allclose(mue_median, 0.30, atol=0.01)
         
         # dictionary of scalars (first 5 cycles)
         blank_dict = {
@@ -96,7 +98,129 @@ class TestSplineMueScipy(unittest.TestCase):
             for well, data in bldata['BS3'].value.iteritems()
         }
         mue_blank_dict = bletl_analysis.get_mue(bldata['BS3'], blank=blank_dict, method='us')
-        numpy.testing.assert_allclose(mue_blank_dict.value.loc[50, 'B03'], 0.369, atol=0.001)
+        mue_median = numpy.median(mue_blank_dict.value.loc[60:75, 'B03'])
+        numpy.testing.assert_allclose(mue_median, 0.38, atol=0.01)
+        return
+
+
+class TestSplineMueCsaps(unittest.TestCase):
+    def test_get_single_spline(self):
+        """Tests the interpolation of backscatters works with an absolute tolerance of <0.1."""
+        bldata = bletl.parse(FP_TESTFILE)
+
+        x, y = bldata['BS3'].get_timeseries('F05')
+
+        spline = bletl_analysis.get_crossvalidated_spline(x, y, method='ucss')
+        
+        self.assertIsInstance(spline, bletl_analysis.UnivariateCubicSmoothingSpline)
+
+        # the last point should be very close
+        numpy.testing.assert_allclose(spline(19.4275), 23.66, atol=0.1)
+
+        # a range of points at the end of the curve
+        numpy.testing.assert_allclose(spline([18.8275, 19.0275, 19.22777778]), [23.75, 23.67, 23.68], atol=0.1)
+        return
+
+    def test_get_mue_wells(self):
+        """Tests that the median growth rate over 15 exponential cycles is calculated with <0.02 absolute deviance."""
+        bldata = bletl.parse(FP_TESTFILE)
+        wells = 'A01,A02,B03,C05'.split(',')
+
+        # automatic blank
+        mue_blank_first = bletl_analysis.get_mue(bldata['BS3'], wells=wells, method='ucss')
+        mue_median = numpy.median(mue_blank_first.value.loc[60:75, 'B03'])
+        numpy.testing.assert_allclose(mue_median, 0.38, atol=0.02)
+
+        # scalar blank for all
+        mue_blank_scalar = bletl_analysis.get_mue(bldata['BS3'], blank=2, wells=wells, method='ucss')
+        mue_median = numpy.median(mue_blank_scalar.value.loc[60:75, 'B03'])
+        numpy.testing.assert_allclose(mue_median, 0.30, atol=0.02)
+        
+        # dictionary of scalars (first 5 cycles)
+        blank_dict = {
+            well : data.iloc[:5].mean()
+            for well, data in bldata['BS3'].value.iteritems()
+            if well in wells
+        }
+        mue_blank_dict = bletl_analysis.get_mue(bldata['BS3'], wells=wells, blank=blank_dict, method='ucss')
+        mue_median = numpy.median(mue_blank_dict.value.loc[60:75, 'B03'])
+        numpy.testing.assert_allclose(mue_median, 0.38, atol=0.02)
+
+        # check value error when the wells dictionary is incorrect
+        with self.assertRaises(ValueError):
+            bletl_analysis.get_mue(bldata['BS3'], wells=wells, blank=dict(A01=3, C02=4), method='ucss')
+        # check value error on invalid blank option
+        with self.assertRaises(ValueError):
+            bletl_analysis.get_mue(bldata['BS3'], wells=wells, blank='last', method='us')
+        return
+
+    def test_get_mue_on_all(self):
+        """Tests that the median growth rate over 15 exponential cycles is calculated with <0.02 absolute deviance."""
+        bldata = bletl.parse(FP_TESTFILE)
+        wells = list('A01,A02,B03,C05'.split(','))
+        bldata['BS3'].time = bldata['BS3'].time[wells]
+        bldata['BS3'].value = bldata['BS3'].value[wells]
+        
+        # automatic
+        mue_blank_first = bletl_analysis.get_mue(bldata['BS3'], method='ucss')
+        mue_median = numpy.median(mue_blank_first.value.loc[60:75, 'B03'])
+        numpy.testing.assert_allclose(mue_median, 0.38, atol=0.02)
+
+        # scalar blank for all
+        mue_blank_scalar = bletl_analysis.get_mue(bldata['BS3'], blank=2, method='ucss')
+        mue_median = numpy.median(mue_blank_scalar.value.loc[60:75, 'B03'])
+        numpy.testing.assert_allclose(mue_median, 0.30, atol=0.02)
+        
+        # dictionary of scalars (first 5 cycles)
+        blank_dict = {
+            well : data.iloc[:5].mean()
+            for well, data in bldata['BS3'].value.iteritems()
+        }
+        mue_blank_dict = bletl_analysis.get_mue(bldata['BS3'], blank=blank_dict, method='ucss')
+        mue_median = numpy.median(mue_blank_dict.value.loc[60:75, 'B03'])
+        numpy.testing.assert_allclose(mue_median, 0.38, atol=0.02)
+        return
+
+
+class TestSplineMethodEquivalence(unittest.TestCase):
+    def test_API_comparable(self):
+        x = numpy.linspace(0, 50, 20)
+        y = numpy.random.normal(x)
+        ucss = bletl_analysis.get_crossvalidated_spline(x, y, method='ucss')
+        self.assertIsInstance(ucss, bletl_analysis.UnivariateCubicSmoothingSpline)
+        der_1 = ucss.derivative(1)
+        der_2 = ucss.derivative(2)
+        der_3 = ucss.derivative(3)
+        self.assertIsInstance(der_1(x), numpy.ndarray)
+        self.assertIsInstance(der_1(5.3), float)
+        self.assertIsNotNone(der_1)
+        self.assertIsNotNone(der_2)
+        self.assertIsNotNone(der_3)
+        self.assertIsNotNone(der_1(x))
+        self.assertIsNotNone(der_2(x))
+        self.assertIsNotNone(der_3(x))
+        return
+
+    def test_ideal_exponetial_mue(self):
+        # generate data of ideal exponential growth
+        mue = 0.352
+        y0 = 0.05
+        t = numpy.linspace(0, 20, 50)
+        y = y0 * numpy.exp(mue*t)
+
+        spline_us = bletl_analysis.get_crossvalidated_spline(t, y, method='us')
+        spline_ucss = bletl_analysis.get_crossvalidated_spline(t, y, method='ucss')
+
+        # test that both spline approximations have residuals of less than 1 % of the signal amplitude
+        numpy.testing.assert_allclose(spline_us(t), y, atol=y.max()*0.03)
+        numpy.testing.assert_allclose(spline_ucss(t), y, atol=y.max()*0.03)
+
+        # test that the median specific growth rate is close to the true value
+        mue_us = spline_us.derivative(1)(t[1:]) / spline_us(t[1:])
+        mue_ucss = spline_ucss.derivative(1)(t[1:]) / spline_us(t[1:])
+
+        numpy.testing.assert_almost_equal(numpy.median(mue_us), mue, decimal=2)
+        numpy.testing.assert_almost_equal(numpy.median(mue_ucss), mue, decimal=2)
         return
 
 
