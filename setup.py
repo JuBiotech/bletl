8e160224dfc2c8aa59d1f472fe1c39fd1ef58f54
Michael Osthege <michael.osthege@outlook.com>
2023-01-01 15:51:38 +0100

    Support Python 3.11

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ if __name__ == "__main__":
             "Programming Language :: Python :: 3.8",
             "Programming Language :: Python :: 3.9",
             "Programming Language :: Python :: 3.10",
+            "Programming Language :: Python :: 3.11",
             "Intended Audience :: Science/Research",
             "License :: OSI Approved :: GNU Affero General Public License v3",
         ],
