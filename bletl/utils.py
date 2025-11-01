JuBiotech/bletl

commit c127211974deb4e4850d06534e29f2a1e0870392

Update df creation in helper function

bletl/utils.py
@@ -1,4 +1,5 @@
 """Contains helper functions that do not depend on other modules within this package."""
+
 import datetime
 import enum
 import pathlib
@@ -35,7 +36,7 @@ def __to_typed_cols__(
             dfout[ncol] = None
         elif issubclass(typ, enum.Enum):
             # Enum types are kept as object-series
-            dfout[ncol] = dfin[ocol].apply(lambda x: typ(x), convert_dtype=False)
+            dfout[ncol] = pandas.Series([typ(x) for x in dfin[ocol]], name=ncol, dtype=object)
         else:
             dfout[ncol] = dfin[ocol].astype(typ)
     return dfout

bletl/__init__.py
@@ -36,4 +36,4 @@ from .types import (
     NoMeasurementData,
 )
 
-__version__ = "1.4.1"
+__version__ = "1.4.2"

commit d857fd57085d72fd53dacc8d4e8e31fd7f03cac9

Fix order and type conversion of `fluidics["reservoir"]` column (#85)
* Run pre-commit on Python 3.13

* Modify test to show that `fluidics["reservoir"]` was broken

* Fix bug in conversion of enum columns

The bug was introduced in https://github.com/JuBiotech/bletl/pull/67
and released in v1.4.2 (2024-06-18).

.github/workflows/pre-commit.yml
@@ -12,5 +12,5 @@ jobs:
     - uses: actions/checkout@v5
     - uses: actions/setup-python@v6
       with:
-        python-version: '3.x'
+        python-version: '3.13'
     - uses: pre-commit/action@v3.0.1

bletl/utils.py
@@ -38,7 +38,9 @@ def __to_typed_cols__(
             dfout[ncol] = None
         elif issubclass(typ, enum.Enum):
             # Enum types are kept as object-series
-            dfout[ncol] = pandas.Series([typ(x) for x in dfin[ocol]], name=ncol, dtype=object)
+            dfout[ncol] = pandas.Series(
+                [typ(x) for x in dfin[ocol]], index=dfin.index, name=ncol, dtype=object
+            )
         else:
             dfout[ncol] = dfin[ocol].astype(typ)
     return dfout

pyproject.toml
@@ -6,7 +6,7 @@ build-backend = "setuptools.build_meta"
 
 [project]
 name = "bletl"
-version = "1.6.1"
+version = "1.6.2"
 description = "Package for parsing and transforming BioLector raw data."
 readme = "README.md"
 requires-python = ">=3.8"

tests/test_core.py
@@ -683,5 +683,8 @@ class TestBLProMF:
     def test_fluidics_source(self):
         fp = dir_testfiles / "BLPro" / "18-FZJ-Test2--2018-02-07-10-01-11.csv"
         bldata = bletl.parse(fp)
-        assert isinstance(bldata.fluidics["reservoir"][0], bletl.FluidicsSource)
+        correct_type = numpy.array(
+            [isinstance(r, bletl.FluidicsSource) for r in bldata.fluidics["reservoir"]]
+        )
+        assert all(correct_type), f"{sum(correct_type)/len(correct_type)} % correct"
         pass